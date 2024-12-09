af76a079810416bb0f6aae9a26dca56bb52ef154
Onur Sahin <sahinonur2000@hotmail.com>
2024-12-09 21:21:02 +0100

    feat: spawn xournal processes concurrently

--- export-pdfs.py
+++ export-pdfs.py
@@ -1,13 +1,16 @@
+import asyncio
 import zipfile
 import subprocess
 import argparse
 import re
+import concurrent.futures
 from pathlib import Path
 
 def main():
     args = get_args()
+    out_dir = Path(args.out_dir)
 
-    Path(args.out_dir).mkdir(exist_ok=True)
+    out_dir.mkdir(exist_ok=True)
 
     # Unzip evaluation
     with zipfile.ZipFile(args.multi_feedback_zip_path, 'r') as zip_ref:
@@ -17,34 +20,49 @@ def main():
 
     # Iterate through submissions
     submissions_dir = Path(args.submissions_path)
-    submissions_dir = next(f for f in Path(args.submissions_path).iterdir() if re.match("Exercise Sheet *", f.name))
-    submissions_dir = next(f for f in submissions_dir.iterdir() if re.match("Submissions", f.name))
-    for submission_dir in submissions_dir.iterdir():
-        print(f"Exporting submission for: {submission_dir.name}")
-        
-        # Find first .xopp file in submission directory
-        try:
-            xournalpp_file = next(f for f in submission_dir.iterdir() if Path(f).suffix == ".xopp")
-        except StopIteration:
-            print("Warning: No Xournal++ file found, skipping")
-            continue
-                    
-        print(f"Xournal++ file found: {xournalpp_file}")
 
-        # Find the corresponding feedback directory
-        try:
-            feedback_dir = next(f for f in out_dir.iterdir() if f.name == submission_dir.name)
-        except StopIteration:
-            print("Error: couldn't find evaluation folder for the submission, skipping")
-            continue
-        
-        # Export pdf using Xournal++
-        pdf_path = feedback_dir.joinpath("feedback.pdf")
-        print(f"Exporting pdf to: {pdf_path}")
-        subprocess.run(["xournalpp", xournalpp_file, "-p", pdf_path])
+    asyncio.run(handle_submissions(submissions_dir, out_dir))
 
     print("Finished exporting")
     print(f"Multi feedback directory can be found in: {out_dir}")
+
+async def handle_submissions(submissions_path: Path, out_dir: Path):
+    submissions_dir = next(f for f in submissions_path.iterdir() if re.match("Exercise Sheet *", f.name))
+    submissions_dir = next(f for f in submissions_dir.iterdir() if re.match("Submissions", f.name))
+
+    tasks = []
+    for submission_dir in submissions_dir.iterdir():
+        task = asyncio.create_task(handle_submision(submission_dir, out_dir))
+        tasks.append(task)
+    
+    await asyncio.wait(tasks)    
+
+async def handle_submision(submission_dir: Path, out_dir: Path):
+    print(f"Exporting submission for: {submission_dir.name}")
+        
+    # Find first .xopp file in submission directory
+    try:
+        xournalpp_file = next(f for f in submission_dir.iterdir() if Path(f).suffix == ".xopp")
+    except StopIteration:
+        print("Warning: No Xournal++ file found, skipping")
+        return
+                
+    print(f"Xournal++ file found: {xournalpp_file}")
+
+    # Find the corresponding feedback directory
+    try:
+        feedback_dir = next(f for f in out_dir.iterdir() if f.name == submission_dir.name)
+    except StopIteration:
+        print("Error: couldn't find evaluation folder for the submission, skipping")
+        return
+    
+    # Export pdf using Xournal++
+    pdf_path = feedback_dir.joinpath("feedback.pdf")
+    print(f"Exporting pdf to: {pdf_path}")
+
+    # Nonblocking call to Xournal++
+    process = await asyncio.create_subprocess_exec("xournalpp", str(xournalpp_file), "-p", str(pdf_path))
+    await process.wait()
 
 def get_args():
     parser = argparse.ArgumentParser(
